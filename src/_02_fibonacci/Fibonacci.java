42e9d7a555e6341acaad41bb507eca3ef20595aa
League Student <league@iMac-37.attlocal.net>
2019-09-07 11:25:14 -0700

    GuessingPIIP:smile"

--- src/_02_fibonacci/Fibonacci.java
+++ src/_02_fibonacci/Fibonacci.java
@@ -1,9 +1,17 @@
 package _02_fibonacci;
 
 public class Fibonacci {
-	int hi = 0;
-	int w = 2;
 	public static void main(String[] args) {
-		
+		int first = 0;
+		int second = 1;
+		int third;
+		System.out.println(first);
+		System.out.println(second);
+		for(int i = 0; i < 12; i++) {
+			third = second+first;
+			System.out.println(third);
+			first = second;
+			second = third;
+		}
 	}
 }
